84c7c62d3ea9445f6f37c349fc40f880fd9726bd
Akshay Kumar Sharma <akshaysharma@192.168.1.2>
2026-05-30 00:59:34 +0530

    Use system theme by default (auto dark/light from OS preference)

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ const App = () => (
   <QueryClientProvider client={queryClient}>
     <ThemeProvider
       attribute="class"
-      defaultTheme="light"
+      defaultTheme="system"
       enableSystem
       disableTransitionOnChange
     >
